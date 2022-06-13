d6407020e1727a1b9af3fe98176f862b139ab89a
AaronMing <29460116@qq.com>
2022-06-14 02:39:34 +0800

    ADD

---
+++ 02/2.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define PRICE 30 
+
+int main() {
+	int num, total;
+	num = 10;
+	total = num * PRICE;
+	cout << "total=" << total << endl;
+
+	return 0; 
+}
