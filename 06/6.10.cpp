f5c74c5823118d51b3306dbbf1cb406e3291aa7c
AaronMingCN <29460116@qq.com>
2022-06-16 19:32:07 +0800

    ADD

--- 06/6.10.cpp
+++ 06/6.10.cpp
@@ -6,8 +6,8 @@ int main() {
 	p2 = str2;
 	for (; *p1 != '\0'; p1++, p2++) *p2 = *p1;
 	*p2 = '\0';
-	//p1 = str1;
-	//p2 = str2;
+	p1 = str1;
+	p2 = str2;
 	cout << "str1 is:" << p1 << endl;
 	cout << "str2 is:" << p2 << endl;
 	
