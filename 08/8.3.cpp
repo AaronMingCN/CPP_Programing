a5f0ab46b10c4ae68d8c8bdf3a74882154c8dc70
AaronMingCN <29460116@qq.com>
2022-06-22 19:00:00 +0800

    ADD

---
+++ 08/8.3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Time {
+	public:
+		void set_time();
+		void show_time();
+	private:	
+		int hour;
+		int minute;
+		int sec;
+}; 
+
+int main() {
+	void set_time(Time &, int hour = 0, int minute = 0, int sec = 0);
+	void show_time(Time &);
+	
+	Time t1;
+	t1.set_time();
+	t1.show_time();
+	
+	Time t2;
+	t2.set_time();
+	t2.show_time();
+	
+	return 0;
+}
+
+
+void Time::set_time() {
+	cin >> hour;
+	cin >> minute;
+	cin >> sec;
+}
+
+void Time::show_time() {
+	cout << hour << ":" << minute << ":" << sec << endl;
+}
+
+
+
+
